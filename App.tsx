73fd1e7817b847ccf60a32a72c484f4d36762e36
Nicolas Deslandes <ndeslandes@live.fr>
2019-03-26 21:56:03 +0000

    Update to React Native 0.59

--- App.tsx
+++ App.tsx
@@ -7,23 +7,83 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import Animated from 'react-native-reanimated';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const { Clock, Value, debug, add, divide, clockRunning, startClock, sin, sub, multiply, stopClock, cond, not, eq, interpolate, set } = Animated;
+type Node<T> = Animated.Node<T>;
+type Adaptable<T> = Animated.Adaptable<T>;
 
 type Props = {};
 export default class App extends Component<Props> {
+  gestureEvent: (...args: any[]) => void;
+  tapGestureEvent: (...args: any[]) => void;
+  _opacity: Node<number>;
+  private _offsetX: Node<number>;
+  _rotation: Animated.Node<number>;
+  _offsetY: Animated.Node<number>;
+
+  constructor(props: Props) {
+    super(props);
+
+    const state = new Value(-1);
+    const tapState = new Value(-1);
+    const dx = new Value(0);
+    const x = new Value(0);
+    const y = new Value(0);
+    const clock = new Clock();
+
+    const dy = multiply(sin(divide(clock, 500)), 200);
+    this._offsetY = cond(clockRunning(clock), add(y, dy), y);
+
+    const ifClockIsNotRunning = (action: Adaptable<number>) => cond(not(clockRunning(clock)), action);
+      
+    this._offsetX =
+      cond(eq(debug('state', state), State.BEGAN), [
+          ifClockIsNotRunning([set(y, sub(y, dy)), startClock(clock)]),
+          add(x, dx)
+        ],
+        cond(eq(state, State.ACTIVE),
+          add(x, dx),
+          [ stopClock(clock), set(y, add(y, dy)), set(x, add(x, dx)) ]
+          )
+        );
+
+    this._opacity = interpolate(this._offsetX,{
+      inputRange: [-200, 200],
+      outputRange: [0, 1],
+      extrapolate: 'clamp',
+    });
+
+    this._rotation = interpolate(this._offsetX,{
+      inputRange: [-200, 200],
+      outputRange: [-3.14159, 3.14159],
+      extrapolate: 'clamp',
+    });
+
+    this.tapGestureEvent = Animated.event([{
+      nativeEvent: { state: tapState }
+    }]);
+    this.gestureEvent = Animated.event([{
+      nativeEvent: { state, translationX: dx }
+    }]);
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <PanGestureHandler onHandlerStateChange={this.gestureEvent}
+          onGestureEvent={this.gestureEvent}>
+          <Animated.View style={[styles.box, {
+            opacity: this._opacity,
+            transform: [
+              {
+                translateX: this._offsetX,
+                translateY: this._offsetY,
+                rotate: this._rotation
+              }
+            ]}]} />
+        </PanGestureHandler>
       </View>
     );
   }
@@ -36,14 +96,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  box: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'red',
   },
 });
